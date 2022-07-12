dd0b682d6c11687d95455bf9c06277b6836f096c
Lucas-Ribeiro-Da-Silva <lucas.r.s2003@hotmail.com>
2022-07-12 08:40:51 -0300

    commit

---
+++ módulo 2/mentoria-typescript/src/desafios/desafio1.ts
@@ -0,0 +1,13 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+// let employee = {};
+// employee.code = 10;
+// employee.name = "John";
+
+
+//resposta
+
+let employee:{code: number, name: string} = {
+    code: 18,
+    name: 'Lucas'
+}
---
+++ módulo 2/mentoria-typescript/src/desafios/desafio2.ts
@@ -0,0 +1,57 @@
+// Como podemos melhorar o esse código usando TS? 
+
+// let pessoa1 = {};
+// pessoa1.nome = "maria";
+// pessoa1.idade = 29;
+// pessoa1.profissao = "atriz"
+
+// let pessoa2 = {}
+// pessoa2.nome = "roberto";
+// pessoa2.idade = 19;
+// pessoa2.profissao = "Padeiro";
+
+// let pessoa3 = {
+//     nome: "laura",
+//     idade: "32",
+//     profissao: "Atriz"
+// };
+
+// let pessoa4 = {
+//     nome = "carlos",
+//     idade = 19,
+//     profissao = "padeiro"
+// }
+
+
+
+//solução
+type pessoa = {
+    nome: string,
+    idade: number,
+    profissao?: string
+}
+
+let pessoa1: pessoa = {
+    nome: "maria",
+    idade: 29,
+    profissao: "atriz"
+};
+
+
+let pessoa2: pessoa = {
+    nome: "roberto",
+    idade: 19,
+    profissao: "Padeiro"
+}
+
+
+let pessoa3: pessoa = {
+    nome: "laura",
+    idade: 32,
+    profissao: "Atriz"
+};
+
+let pessoa4: pessoa = {
+    nome: "carlos",
+    idade: 19
+}
